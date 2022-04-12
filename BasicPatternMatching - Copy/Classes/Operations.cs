a2ddb9c73f5201962e2893c58a7e5586274ce936
Karen Payne <paynekaren@comcast.net>
2022-04-12 13:30:08 -0700

    local NuGet package for config library

--- BasicPatternMatching - Copy/Classes/Operations.cs
+++ BasicPatternMatching - Copy/Classes/Operations.cs
@@ -1,13 +1,10 @@
 ﻿
-using ConfigurationLibrary.Classes;
-
 namespace BasicPatternMatching.Classes
 {
     public class Operations
     {
         public static string ConnectionString() => ConfigurationHelper.ConnectionString();
 
-        public static Settings ApplicationSettings() => Configuration.ApplicationSettings();
-
+        public static Settings ApplicationSettings() => ConfigurationHelper.ApplicationSettings();
     }
 }
---
+++ BasicPatternMatching/Classes/Configuration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace BasicPatternMatching.Classes
+{
+    public class Configuration
+    {
+        public static Settings ApplicationSettings()
+        {
+            var configuration = Builder();
+            return configuration.GetSection("Settings").Get<Settings>();
+        }
+
+        private static IConfigurationRoot Builder()
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            var configuration = builder.Build();
+            return configuration;
+        }
+    }
+}
